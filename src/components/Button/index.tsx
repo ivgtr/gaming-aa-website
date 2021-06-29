60653c776a0fab83c2d515e2a8214c4c26a13f09
ivgtr <ivgtr.me@gmail.com>
2021-06-29 19:38:26 +0900

    :ambulance: fix: styleの修正

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const Button: React.FC<Props> = (props) => {
   return (
     <button
-      className="h-8 leading-8 ml-2 w-16 rounded-md border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      className="h-8 leading-8 ml-2 px-2 rounded-md border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
       onClick={props.onClick}
     >
       {props.children}
