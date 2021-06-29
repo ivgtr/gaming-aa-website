4c50a00280be8b1d5aa2825f51c82a2fe8ebcabd
ivgtr <ivgtr.me@gmail.com>
2021-06-29 20:12:02 +0900

    :lipstick: change: style調整

--- src/components/PageContents/index.tsx
+++ src/components/PageContents/index.tsx
@@ -5,6 +5,9 @@ import classes from './PageContents.module.scss'
 
 export const PageContents: React.VFC = () => {
   const [AA, setAA] = useState<string>('')
+  const [palette, setPalette] = useState<string[]>(['#40e0d0', '#ff8c00', '#ff0080'])
+
+  const gradient = `-webkit-linear-gradient(0deg, ${palette.join(',')})`
 
   return (
     <div className="px-6 overflow-hidden">
@@ -18,12 +21,12 @@ export const PageContents: React.VFC = () => {
           value={AA}
           onChange={(e) => {
             setAA(e.target.value)
-            // console.log(encodeURI(e.target.value))
+            console.log(encodeURI(e.target.value))
           }}
         ></textarea>
       </div>
       <div>
-        {sampleJson.samples.map((sample, index) => {
+        {sampleJson.aa_samples.map((sample, index) => {
           return (
             <Button
               onClick={() => {
@@ -43,11 +46,36 @@ export const PageContents: React.VFC = () => {
           リセット
         </Button>
       </div>
+      <div className="mt-4">
+        {sampleJson.color_samples.map((sample, i) => {
+          return (
+            <div
+              key={i}
+              className="flex"
+              onClick={() => {
+                setPalette(sample.palette)
+                console.log(gradient)
+              }}
+            >
+              {sample.palette.map((color, j) => {
+                return <span key={j} style={{ backgroundColor: color }} className="h-6 w-6"></span>
+              })}
+            </div>
+          )
+        })}
+      </div>
       <div>
         <p className="py-4">↓</p>
       </div>
       <div>
-        <p className={classes.gaming}>{AA}</p>
+        <p className={classes.gaming}>
+          {AA.split('\n').map((str, index) => (
+            <React.Fragment key={index}>
+              {str}
+              <br />
+            </React.Fragment>
+          ))}
+        </p>
       </div>
     </div>
   )
