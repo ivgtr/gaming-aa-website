8e8f9c866f1d0a05956096ea35fecbdf3bb88818
ivgtr <ivgtr.me@gmail.com>
2021-06-30 13:58:41 +0900

    :seedling: format: classnameの記述を変更

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,7 +10,19 @@ export const Button: React.FC<Props> = (props) => {
   return (
     <button
       className={classNames(
-        "h-8 leading-8 ml-2 px-2 text-gray-50 rounded-md border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50",
+        "h-8",
+        "leading-8",
+        "ml-2",
+        "px-2",
+        "text-gray-50",
+        "rounded-md",
+        "border",
+        "border-gray-500",
+        "shadow-sm",
+        "focus:border-indigo-300",
+        "focus:ring",
+        "focus:ring-indigo-200",
+        "focus:ring-opacity-50",
         classes.button
       )}
       onClick={props.onClick}
--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,7 +5,7 @@ import classes from "./PageHeader.module.scss";
 export const PageHeader = () => {
   return (
     <header>
-      <h1 className={classNames("p-4 font-bold text-xl text-gray-50", classes.title)}>
+      <h1 className={classNames("p-4", "font-bold", "text-xl", "text-gray-50", classes.title)}>
         ゲーミング AA
       </h1>
     </header>
